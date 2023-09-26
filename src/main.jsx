2145db3caf897c23909dd3225a7e29796b23a003
gabicilfone <cilfonegabriel@gmail.com>
2023-09-26 15:30:50 -0300

    Create a Loader

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import NewClient from './pages/NewClient'
-import Index from './pages/Index.jsx'
+import Index, {loader as clientsLoader} from './pages/Index.jsx'
 
 
 const router = createBrowserRouter([
@@ -15,6 +15,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <Index />,
+        loader: clientsLoader
       },
       {
         path: '/clients/new',
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,16 @@
+export function loader() {
+
+  return 'From Loader';
+}
+
 
 function index() {
+
   return (
-    <div>
-      Clients
-    </div>
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clients</h1>
+      <p className="mt-3">Manage your clients</p>
+    </>
   )
 }
 

53ada966d22523f8e2dc714101353274a4d40550
gabicilfone <cilfonegabriel@gmail.com>
2023-09-26 16:08:17 -0300

    Show all information

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -4,9 +4,31 @@ function Client({client}) {
     const { nombre, empresa, email, telefono, id } = client;
 
     return (
-        <tr>
+        <tr className="border-b">
+            <td className="p-6 space-y-2">
+                <p className="text-2xl text-gray-800">{nombre}</p>
+                <p>{empresa}</p>
+            </td>
+
             <td className="p-6">
-                {nombre}
+                <p className="text-gray-600"><span className="text-gray-800 uppercase font-bolt">Email: </span>{email}</p>
+                <p className="text-gray-600"><span className="text-gray-800 uppercase font-bolt">Tel: </span>{telefono}</p>
+            </td>
+
+            <td className="p-6 flex gap-3">
+                <button
+                    type="button"
+                    className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+                >
+                    Edit
+                </button>
+
+                <button
+                    type="button"
+                    className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+                >
+                    Delete
+                </button>
             </td>
         </tr>
     )
